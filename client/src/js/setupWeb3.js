e2e9124bfae0281bc5e4bd1d34fd9066af00a02d
DJ Rosenbaum <djrosenbaum@gmail.com>
2019-09-11 19:26:38 -0400

    enable ethereum

--- client/src/js/setupWeb3.js
+++ client/src/js/setupWeb3.js
@@ -2,6 +2,9 @@ import { ethers } from 'ethers';
 
 export default async function setupWeb3() {
   if (window.ethereum || window.web3) {
+    if (window.ethereum) {
+      window.ethereum.enable()
+    }
     window.dapp.provider = new ethers.providers.Web3Provider(web3.currentProvider);
   } else {
     console.log('no provider detected');
